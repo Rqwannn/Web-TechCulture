49682b1cb51985fe680858673f72d9fcf409e661
Rqwannn <mohwannn1@gmail.com>
2021-09-05 11:16:37 +0700

    Btn Slide Up

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,8 @@
 window.addEventListener("scroll", function () {
     const Navbar = document.querySelector(".navbar");
-  
+    const WrapperCircleUp= document.querySelector('.WrapperCircleUp');
+
+    WrapperCircleUp.classList.toggle('SlideUpOpacity', window.scrollY > 0);
     Navbar.classList.toggle("PassScroll", window.scrollY > 0);
   });
 
@@ -9,3 +11,10 @@ window.addEventListener("scroll", function () {
   Menu.addEventListener("click", () => {
       Menu.classList.toggle("open");
   });
+
+  function ScroolTop(){
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      })
+  }
